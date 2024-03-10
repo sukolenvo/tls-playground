295351eba0dadef3831b1ad05e9ae71873ff3ec1
Vadym <sukolenvo@gmail.com>
2024-03-10 21:32:19 +0800

    build_schedule_key

--- src/main.cpp
+++ src/main.cpp
@@ -53,3 +53,50 @@ void schedule_key_rotl(std::array<unsigned char, 7> &key)
 	key[5] = (0x7F & copy[5]) << 1 | (0x80 & copy[6]) >> 7;
 	key[6] = (0x7F & copy[6]) << 1 | (0x08 & copy[3]) >> 3;
 }
+
+const auto schedule_key_permutation_table = std::vector<unsigned int> {
+	57, 49, 41, 33, 25, 17, 9, 1,
+	58, 50, 42, 34, 26, 18, 10, 2,
+	59, 51, 43, 35, 27, 19, 11, 3,
+	60, 52, 44, 36,
+	63, 55, 47, 39, 31, 23, 15, 7,
+	62, 54, 46, 38, 30, 22, 14, 6,
+	61, 53, 45, 37, 29, 21, 13, 5,
+	28, 20, 12, 4
+};
+
+const auto des_expansion_table = std::vector<unsigned int>{
+	32, 1, 2, 3, 4, 5,
+	4, 5, 6, 7, 8, 9,
+	8, 9, 10, 11, 12, 13,
+	12, 13, 14, 15, 16, 17,
+	16, 17, 18, 19, 20, 21,
+	20, 21, 22, 23, 24, 25,
+	24, 25, 26, 27, 28, 29,
+	28, 29, 30, 31, 32, 1
+};
+
+std::array<std::array<unsigned char, 7>, 16> build_schedule_key(const std::array<unsigned char, 8> &key)
+{
+	std::array<unsigned char, 7> permuted_key;
+	permute(permuted_key, key, schedule_key_permutation_table);
+	std::array<std::array<unsigned char, 7>, 16> schedule_keys{};
+	for (int round = 0; round < 16; round++ ) {
+		schedule_key_rotl(permuted_key);
+		if ( !( round <= 1 || round == 8 || round == 15 ) )
+		{
+			// Rotate twice except in rounds 1, 2, 9 & 16
+			schedule_key_rotl( permuted_key );
+		}
+		schedule_keys[round] = permuted_key;
+	}
+	return schedule_keys;
+}
+
+void des_block_encrypt(const std::array<unsigned char, 8> &input_block,
+					   std::array<unsigned char, 8> &output_block,
+					   const std::array<unsigned char, 8> &key)
+{
+	permute(output_block, input_block, initial_permute_table);
+	const auto schedule_keys = build_schedule_key(key);
+}
--- src/main.hpp
+++ src/main.hpp
@@ -32,29 +32,37 @@ void set_bit(std::vector<unsigned char> &src, unsigned int index);
 void clear_bit(std::vector<unsigned char> &src, unsigned int index);
 
 const auto initial_permute_table = std::vector<unsigned int>{
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9, 1,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
+	58, 50, 42, 34, 26, 18, 10, 2,
+	60, 52, 44, 36, 28, 20, 12, 4,
+	62, 54, 46, 38, 30, 22, 14, 6,
+	64, 56, 48, 40, 32, 24, 16, 8,
+	57, 49, 41, 33, 25, 17, 9, 1,
+	59, 51, 43, 35, 27, 19, 11, 3,
+	61, 53, 45, 37, 29, 21, 13, 5,
+	63, 55, 47, 39, 31, 23, 15, 7
 };
 
-const auto invert_permute_table = std::vector<unsigned int> {
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25
+const auto invert_permute_table = std::vector<unsigned int>{
+	40, 8, 48, 16, 56, 24, 64, 32,
+	39, 7, 47, 15, 55, 23, 63, 31,
+	38, 6, 46, 14, 54, 22, 62, 30,
+	37, 5, 45, 13, 53, 21, 61, 29,
+	36, 4, 44, 12, 52, 20, 60, 28,
+	35, 3, 43, 11, 51, 19, 59, 27,
+	34, 2, 42, 10, 50, 18, 58, 26,
+	33, 1, 41, 9, 49, 17, 57, 25
 };
 
-void permute(std::vector<unsigned char> &target, std::vector<unsigned char> &src, std::vector<unsigned int> permute_table);
+void permute(std::vector<unsigned char> &target,
+			 std::vector<unsigned char> &src,
+			 std::vector<unsigned int> permute_table);
 
 void schedule_key_rotl(std::array<unsigned char, 7> &key);
+
+std::array<std::array<unsigned char, 7>, 16> build_schedule_key(const std::array<unsigned char, 8> &key);
+
+void des_block_encrypt(const std::array<unsigned char, 8> &input_block,
+					   std::array<unsigned char, 8> &output_block,
+					   const std::array<unsigned char, 8> &key);
 
 #endif //TLS_PLAYGROUND_MAIN_HPP
