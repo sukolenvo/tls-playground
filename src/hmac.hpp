a9e5e2da2b63766e2b8a0da764e39ac50885f136
vadym <vsukolen@anaqua.com>
2024-04-05 06:45:30 +0800

    hmac md5; tls pseudo random function

--- src/hmac.hpp
+++ src/hmac.hpp
@@ -8,4 +8,8 @@ std::array<unsigned char, 32> hmac_sha256(
 		const std::vector<unsigned char> &input,
 		const std::vector<unsigned char> &key);
 
+std::array<unsigned char, 16> hmac_md5(
+		const std::vector<unsigned char> &input,
+		const std::vector<unsigned char> &key);
+
 #endif //TLS_PLAYGROUND_HMAC_HPP
